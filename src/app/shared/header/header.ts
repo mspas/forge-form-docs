3ca5dfad2a1013ccfdcad5052edc4cf8fa62ccb0
Marcin Spasinski <marcin7789@gmail.com>
2026-06-25 14:00:25 +0200

    Add npm link to the header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -8,12 +8,13 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
 import { Logo } from '../logo/logo';
 import { GithubIcon } from '../github-icon/github-icon';
+import { NpmIcon } from '../npm-icon/npm-icon';
 import { DocsContentService } from '../../pages/docs/docs-content.service';
-import { GITHUB_URL, NPM_INSTALL } from '../site';
+import { GITHUB_URL, NPM_URL, NPM_INSTALL } from '../site';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, Logo, GithubIcon],
+  imports: [RouterLink, Logo, GithubIcon, NpmIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,6 +23,7 @@ export class Header {
   private readonly content = inject(DocsContentService);
 
   readonly githubUrl = GITHUB_URL;
+  readonly npmUrl = NPM_URL;
   readonly install = NPM_INSTALL;
 
   /** Version + license, sourced from user-guide.md (undefined until loaded). */
